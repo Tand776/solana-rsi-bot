26439bb41f7aae7ad1a9e6d3c93ad5ab7f3458f6
Tand776 <tandohstephen766@gmail.com>
2026-03-27 14:31:33 -0800

    Delete convert.js

--- convert.js
+++
@@ -1,9 +0,0 @@
-const bs58 = require("bs58").default
-const { Keypair } = require("@solana/web3.js")
-
-const secret = "32t7G68mpGhTkK4WNxVukz9XMueVS6SNtQnGmhV8nf1gYysZir48auhWukU68qE3LkwujEEvreJo2sLRFNwZuu5J"
-
-const decoded = bs58.decode(secret)
-const wallet = Keypair.fromSecretKey(decoded)
-
-console.log(JSON.stringify(Array.from(wallet.secretKey)))
